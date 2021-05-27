1beffc74299635b3a5147fadc2f6f5a26b76be2f
Yujin Yano <northwindapps@gmail.com>
2021-05-27 17:56:30 +0900

    Initial Commit

---
+++ xlsxParser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  xlsxParser
+//
+//  Created by yujin on 2021/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ xlsxParser/xlsxParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  xlsxParserApp.swift
+//  xlsxParser
+//
+//  Created by yujin on 2021/05/27.
+//
+
+import SwiftUI
+
+@main
+struct xlsxParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
